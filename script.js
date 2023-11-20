f238fc240a3af767f10f0d6709f9acaa00532c93
baltata <128985806+baltata@users.noreply.github.com>
2023-11-20 15:10:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+console.log('testitest2');
 const sheetId = '1EceUR6V_uozN0fAkYTE_p9NHLIew8OBOI_Ab_10Z490';
 const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
 const sheetName = 'user-data';
@@ -6,13 +8,15 @@ const url = `${base}&sheet=${sheetName}&tq=${query}`
 const data = []
 document.addEventListener('DOMContentLoaded', init)
 const output = document.querySelector('.output')
+//console.log("output is :", output);
+//console.log("url est :", url)
 function init() {
     fetch(url)
         .then(res => res.text())
         .then(rep => {
             //Remove additional text and extract only JSON:
             const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
-            console.log(rep)
+           // console.log(rep)
             const colz = [];
             const tr = document.createElement('tr');
             //Extract column labels
@@ -25,7 +29,7 @@ function init() {
                     tr.appendChild(th);
                 }
             })
-         //   output.appendChild(tr);
+            //   output.appendChild(tr);
             //extract row data:
             jsonData.table.rows.forEach((rowData) => {
                 const row = {};
@@ -35,17 +39,18 @@ function init() {
                 data.push(row);
             })
             processRows(data);
-            
-            var liste=document.querySelectorAll('.exercice');
+
+            var liste = document.querySelectorAll('.exercice');
             ;
+           // console.log("la liste traitee est :", traitementListe(liste));
             injectHTML(traitementListe(liste));
-       
-          
-                
+
+
+
         })
 
 
-     
+
 
 
 
@@ -54,55 +59,133 @@ function init() {
 
 
 }
-function injectHTML (liste) {
-for (var i = 0, len = liste.length; i < len; i++) {
-    var isThePageBeingEdited=document.querySelector("body").id;
-    if (isThePageBeingEdited!="page-mod-book-edit"){
-    liste[i].insertAdjacentHTML('beforeend','<div class="blur"> <div class="gosabonner">Cet exercice est réservé à nos utilisateurs premium &#128081. <br><a class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div>')
-}}
-}
+function injectHTML(liste) {
+    const exercicesAvecBlur = {};
+    const isblurred={};
 
-
-
-function traitementListe (liste) {
-    var listeTraitee = [];
-
-    for (let i = 0; i<liste.length; i++) {
-        var id = liste[i].id;
-       if (checkStatus(id)==1) {
-     listeTraitee.push(liste[i])
-       }
+    // fill the fiches that are already displayed when the page loads
+    var fiche = document.querySelectorAll(".fiche");
+    for (let i = 0; i < fiche.length; ++i) {
+        identifiantFiche = fiche[i].id;
+        console.log("identifiantFiche est :", identifiantFiche)
+        console.log("checkStatus(identifiantFiche) est :", checkStatus(identifiantFiche))
+        if (checkStatus(identifiantFiche)) {
+            
+                fiche[i].insertAdjacentHTML('beforeend', ' <div class="blur"style="display:grid"> <div class="gosabonner">Pour voir cette <b>fiche</b> il faut un compte premium 👑. <br><a target="_parent" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>');
+                exercicesAvecBlur[identifiantFiche] = true;
+            
+        }
     }
-    console.log(listeTraitee)
-    return listeTraitee
+
+
+
+
+    ///// FILL THE CORRECTIONS THAT ARE ALREADY DISPLAYED WHEN THE PAGE LOADS
+
+    for (var i = 0, len = liste.length; i < len; i++) {
+
+        var contenairexercice = document.querySelectorAll(".exercice iframe");
+        for (let i = 0; i < contenairexercice.length; ++i) {
+            //console.log("contenairexercice[i] est :", contenairexercice[i])
+
+        
+                identifiantExercice = contenairexercice[i].parentElement.id;
+                if (checkStatus(identifiantExercice)) {
+                    if (contenairexercice[i].contentWindow.document.body.querySelector('.outcome')) {
+                        
+                       
+
+
+                    var correction = contenairexercice[i].contentWindow.document.body.querySelector('.outcome');
+                    console.log("correction est :", correction)
+
+
+                    if (!exercicesAvecBlur[identifiantExercice]) {
+                        console.log("l'exercice est censé être flouté  :", correction)
+                            correction.insertAdjacentHTML('beforeend', ' <div class="blur"style="display:grid"> <div class="gosabonner">Pour voir cette <b>correction</b> ou <b>recommencer</b> cet exercice il faut un compte premium 👑. <br><a target="_parent" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>');
+                            exercicesAvecBlur[identifiantExercice] = true;}
+
+
+
+                        }
+                    }};}
+
+////// ADD AN EVENT LISTENER TO DISPLAY THE BLUR WHEN IFRAMES LOAD
+
+        for (var i = 0, len = liste.length; i < len; i++) {
+           
+            var isThePageBeingEdited = document.querySelector("body").id;
+            if (isThePageBeingEdited != "page-mod- book-edit") {
+            //    console.log("DEBUT DU SSSS");
+                var contenairexercice = document.querySelectorAll(".exercice iframe");
+                for (let i=0;i<contenairexercice.length;i++) {
+
+                    contenairexercice[i].addEventListener("load", () => {
+                        identifiantExercice = contenairexercice[i].parentElement.id;
+                        if (checkStatus(identifiantExercice)) {
+                            if (contenairexercice[i].contentWindow.document.body.querySelector('.outcome')) {
+                                
+
+if (!isblurred[identifiantExercice]) {
+
+                            var correction = contenairexercice[i].contentWindow.document.body.querySelector('.outcome');
+                                    correction.insertAdjacentHTML('beforeend', ' <div class="blur"style="display:grid"> <div class="gosabonner">Pour voir cette <b>correction</b> ou <b>recommencer</b> cet exercice il faut un compte premium 👑. <br><a target="_parent" class="awhite" href="https://galilee.ac/local/membership/plan.php"> <div class="whitebutton"><b> Nos offres</b></div></a> </div></div>')
+                                    isblurred[identifiantExercice]=true;
+}
+
+
+                                }
+                            }});}
+
+                }
+            }
+
+
+        };
+
     
 
+
+
+
+
+
+
+function traitementListe(liste) {
+    var listeTraitee = [];
+
+    for (let i = 0; i < liste.length; i++) {
+        var id = liste[i].id;
+        if (checkStatus(id) == 1) {
+            listeTraitee.push(liste[i])
+        }
+    }
+   // console.log("la liste traitee est:", listeTraitee)
+    return listeTraitee
+
+
 }
 
-function checkStatus (id) {
- // check the status of each exercice to see whether it is paying or not
- for (let i = 0; i < data.length; i++) {
-    if (data[i].id==id) {        
-         var retour = data[i].Payant
+function checkStatus(id) {
+    // check the status of each exercice to see whether it is paying or not
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id == id) {
+            var retour = data[i].Payant
         }
-      }
-      return retour;
+    }
+    return retour;
 }
- 
+
 function processRows(json) {
     json.forEach((row) => {
         const tr = document.createElement('tr');
         const keys = Object.keys(row);
-  
+
         keys.forEach((key) => {
             const td = document.createElement('td');
-          td.textContent = row[key];
+            td.textContent = row[key];
             tr.appendChild(td);
         })
-      //  output.appendChild(tr);
+        //  output.appendChild(tr);
     })
 }
-
-
-
-
